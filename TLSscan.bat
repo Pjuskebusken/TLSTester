c80993025e8e9b7b7d05203472d3a1144684dce5
Pjuskebusk <89973993+Pjuskebusken@users.noreply.github.com>
2026-09-22 08:55:52 +0200

    Cahnged URL -> Hostname

--- TLSscan.bat
+++ TLSscan.bat
@@ -9,6 +9,6 @@ if "%siteUrl%"=="" (
     exit /b 1
 )
 
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0TLSscan.ps1" -URL "%siteUrl%"
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0TLSscan.ps1" -Hostname "%siteUrl%"
 
 pause
